409a4b382b35d12088d8bc105cd8c34a87055ba1
tm940911 <68036513+tm940911@users.noreply.github.com>
2020-07-25 14:29:53 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo test
+sudo touch /var/webserver_log/unauthorized.log
+sudo chmod 666 /var/webserver_log/unauthorized.log
+
+sudo cat /var/log/auth.log | grep "sshd" | grep -v "sudo" | grep -E "Disconnected from authenticating user|Connection closed by invalid user|Failed password" | while read line
+do
+        date=`echo $line | grep -oE "[a-zA-Z]* [0-9]{1,2} [0-9][0-9]:[0-9][0-9]:[0-9][0-9]"`
+        ip=`echo $line | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}"`
+        country=`geoiplookup $ip | sed 's/^.*, //'`
+        echo $ip $country $date >> /var/webserver_log/unauthorized.log
+done
