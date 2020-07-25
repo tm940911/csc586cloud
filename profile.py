d611e383fcd2fc590b4a8715b6d6afa64732e52c
tm940911 <68036513+tm940911@users.noreply.github.com>
2020-07-25 11:20:27 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -5,28 +5,20 @@ import geni.rspec.igext as IG
 pc = portal.Context()
 request = pc.makeRequestRSpec()
 
-tourDescription = \
-"""
-This profile provides a three-node set to study NFS/SSO. One node will be the LDAP server, one node will be the NFS server, 
-and the remaining node is an NFS client. Both NFS nodes will be authenticated using LDAP. 
-"""
-#
-# Setup the Tour info with the above description and instructions.
-#  
+tourDescription = "Assignment 2"
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-for i in range(3):
+for i in range(2):
   if i == 0:
-    node = request.XenVM("ldapserver")    
-  elif i == 1:
-    node = request.XenVM("nfsserver")
-  else:
-    node = request.XenVM("nfsclient")
-  node.routable_control_ip = "true"  
+    node = request.XenVM("webserver")
+    node.routable_control_ip = "true"
+ else:
+    node = request.XenVM("observer")
+    node.routable_control_ip = "false"
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
