573c568521caf34524409fa4889734f05f878073
tm940911 <68036513+tm940911@users.noreply.github.com>
2020-07-25 16:06:58 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo test
+#!/bin/bash
+
+new_access=`sudo cat /var/webserver_monitor/unauthorized.log | grep -E "$(date -d '-1 hour' +'%b %d %H')"`
+
+if [ -z "$new_access" ]; then
+        message="No unauthorized access."
+else
+        message=$new_access
+fi
+
+echo $message | mail -s "Access Update" tandmmail2@gmail.com
