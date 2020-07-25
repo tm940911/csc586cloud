7179154c00ebe5e1db8100a250b4dd1867548c6b
tm940911 <68036513+tm940911@users.noreply.github.com>
2020-07-25 15:13:20 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -3,7 +3,11 @@
 sudo touch /var/webserver_log/unauthorized.log
 sudo chmod 666 /var/webserver_log/unauthorized.log
 
-sudo cat /var/log/auth.log | grep "sshd" | grep -v "sudo" | grep -E "Disconnected from authenticating user|Connection closed by invalid user|Failed password" | while read line
+sudo cat /var/log/auth.log \
+        | grep "sshd" \
+        | grep -v "sudo" \
+        | grep -E "^$(date -d '-1 minute' +'%b %d %H:%M')|^$(date -d '-2 minute' +'%b %d %H:%M')|^$(date -d '-3 minute' +'%b %d %H:%M')|^$(date -d '-4 minute' +'%b %d %H:%M')|^$(date -d '-5 minute' +'%b %d %H:%M')" \
+        | grep -E "Disconnected from authenticating user|Connection closed by invalid user|Failed password" | while read line
 do
         date=`echo $line | grep -oE "[a-zA-Z]* [0-9]{1,2} [0-9][0-9]:[0-9][0-9]:[0-9][0-9]"`
         ip=`echo $line | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}"`
